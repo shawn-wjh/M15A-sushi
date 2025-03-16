c726c1f4a42a02abb0adbc4c4b84f29fa8980459
Shawn Woo <z5604575@ad.unsw.edu.au>
2025-03-16 20:32:42 +1100

    adjusted express req size limit

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ const app = express();
 // Middleware
 app.use(cors()); // Enable CORS for all routes
 // when user sends a JSON request, express.json() will parse the JSON body and attach it to the request object (req.body)
-app.use(express.json()); // Parse JSON bodies
+app.use(express.json({ limit: '10mb' })); // Parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies (for form data)
 
 // Use routes
